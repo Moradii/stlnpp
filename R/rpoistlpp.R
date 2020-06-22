b17c091f2001205c2fe807553ddc1a93c66ce155
Moradii <m2.moradi@yahoo.com>
2020-06-22 14:16:59 +0200

    Simulating space-time poisson from a list of intensity images

--- R/rpoistlpp.R
+++ R/rpoistlpp.R
@@ -1,33 +1,30 @@
 #' @export
 rpoistlpp <-  function(lambda,a,b,L,check=FALSE,lmax=NULL,nsim=1){
-
+  
   if (!inherits(L,"linnet"))   stop("L should be a linear network")
-
+  
   if (a >= b)  stop("lower bound must be smaller than upper bound")
-
-  if (!is.numeric(lambda) & !is.function(lambda))
-    stop(" lambda should be a number or a function")
-
- if(nsim > 1) {
-   out <- list()
-   for (i in 1:nsim) {
-     out[[i]] <- rpoislpp(lambda,a=a,b=b,L=L,nsim=1)
-   }
-   return(out)
- }
-
+  
+  if (!is.numeric(lambda) & !is.function(lambda) & !any(class(lambda)=="stlppint"))
+    stop(" lambda should be a number, a function, or an object of class stlppint")
+  
+  if(nsim > 1) {
+    out <- list()
+    for (i in 1:nsim) {
+      out[[i]] <- rpoislpp(lambda,a=a,b=b,L=L,nsim=1)
+    }
+    return(out)
+  }
+  
   if (is.numeric(lambda)){
+    
     n <- rpois(1,lambda*volume(L)*(b-a))
     X <- runiflpp(n,L)
     t <- runif(npoints(X),a,b)
     stlpp <- data.frame(x=X$data$x,y=X$data$y,t)
-
   }
-
-
-
-  else {
-
+  else if(is.function(lambda)){
+    
     if(is.null(lmax)){
       Llines <- as.psp(L)
       linemask <- as.mask.psp(Llines)
@@ -51,36 +48,63 @@ rpoistlpp <-  function(lambda,a,b,L,check=FALSE,lmax=NULL,nsim=1){
       grid <- lpp(df,L)
       grid <- unique(grid)
       t0 <- runif(npoints(grid),a,b)
-
+      
       lmax=max(lambda(grid$data$x,grid$data$y,t0))
     }
-
-
+    
+    
     mean <- lmax*volume(L)*(b-a)
-
+    
     n <- rpois(1,mean)
-
+    
     unipoint <- runiflpp(n,L)
-
+    
     hlpp <- cbind(unipoint$data$x,unipoint$data$y,runif(n,a,b))
-
+    
     prob <- lambda(hlpp[,1],hlpp[,2],hlpp[,3])/lmax
-
+    
     if(check) {
       if(any(prob < 0))
         warning("Negative values of lambda obtained")
       if(any(prob > 1))
         warning("lmax is not an upper bound for lambda")
     }
-
+    
     u <- runif(length(hlpp[,1]))
-
+    
     retain <-  (u <= prob)
-
+    
     stlpp <- hlpp[retain,]
     stlpp <- data.frame(x=stlpp[,1],y=stlpp[,2],t=stlpp[,3])
-
   }
+  else if(any(class(lambda)=="stlppint")){
+    
+    lmax <- max(unlist(lapply(lambda, max)))
+    
+    mean <- lmax*volume(L)*(b-a)
+    
+    n <- rpois(1,mean)
+    
+    unipoint <- runiflpp(n,L)
+    
+    hlpp <- stlpp(unipoint,T=runif(npoints(unipoint),a,b),L=L)
+    
+    prob <- lambda[hlpp]/lmax
+    
+    if(check) {
+      if(any(prob < 0))
+        warning("Negative values of lambda obtained")
+      if(any(prob > 1))
+        warning("lmax is not an upper bound for lambda")
+    }
+    
+    u <- runif(npoints(hlpp))
+    
+    retain <-  (u <= prob)
+    
+    stlpp <- hlpp[retain]
+  }
+  
   out <- ppx(data=stlpp,domain = L,coord.type = c("s","s","t"))
   class(out) <- c("stlpp","ppx")
   out$time <- c(a,b)
